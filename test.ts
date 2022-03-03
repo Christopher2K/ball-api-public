d67bf7f1b0a019133ea233d88b3ee9cfe353268a
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-03-03 17:56:56 -0500

    test: setup testing infrastructure

---
+++ test.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import { configure } from "japa";
+import { join } from "path";
+import getPort from "get-port";
+import sourceMapSupport from "source-map-support";
+import type { HttpServer } from "@adonisjs/core/build/src/Ignitor/HttpServer";
+
+import {
+  supabaseLocalStart,
+  supabaseLocalStop,
+  runMigrations,
+  resetSupabaseDb,
+} from "./tests/utils";
+
+process.env.NODE_ENV = "testing";
+process.env.ADONIS_ACE_CWD = join(__dirname);
+sourceMapSupport.install({ handleUncaughtExceptions: false });
+
+let httpServer: HttpServer | null = null;
+
+async function startHttpServer() {
+  const { Ignitor } = await import("@adonisjs/core/build/src/Ignitor");
+  process.env.PORT = String(await getPort());
+  httpServer = new Ignitor(__dirname).httpServer();
+  await httpServer.start();
+}
+
+async function stopHttpServer() {
+  if (httpServer !== null) {
+    await httpServer.close();
+  }
+}
+
+configure({
+  files: ["tests/e2e/**/*.spec.ts"],
+  before: [supabaseLocalStart, runMigrations, startHttpServer],
+  after: [stopHttpServer, resetSupabaseDb, supabaseLocalStop],
+});
